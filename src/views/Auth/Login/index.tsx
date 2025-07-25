5326fa347162d29e76621f5532b7fa4955e49be7
AchmadTS <achmadtirtosudirosudiro@gmail.com>
2025-07-25 08:24:54 +0700

    feat: tailwindcss

--- src/views/Auth/Login/index.tsx
+++ src/views/Auth/Login/index.tsx
@@ -10,7 +10,7 @@ const LoginViews = () => {
 
   return (
     <div className={styles.login}>
-      <h1 className="big">Login Page</h1>
+      <h1 className="text-3xl">Login Page</h1>
       <button onClick={() => handleLogin()}>Login</button>
       <p style={{color: "red", border: "1px solid red", borderRadius: "10px"}}>
         Belum punya akun? registrasi <Link href={"/auth/register"}>disini</Link>
